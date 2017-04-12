25c682d673d5dfcf5cb54442b4d8627086e03b05
wangyan <2025764074@qq.com>
2017-04-12 11:16:23 +0800

    删除调试语句

--- gulpfile.js
+++ gulpfile.js
@@ -51,7 +51,6 @@ gulp.task('data:pre', () => {
     const _ = require('lodash');
     let obj = { };
     let promiseList = [ ];
-    console.log('111');
 
     filelist.forEach((filepath) => {
         let ref = filepath.match(/([\w_-]+)[\\\/]([\w_-]+)[\\\/]([\w_-]+)$/);

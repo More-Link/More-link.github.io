20cfc52a6648678818fdd04e0bfc66cebcea9d3f
Doubley Yang <doubley_yang@epam.com>
2025-07-11 11:53:47 +0800

    chore: cronjob to update demo page

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,8 @@ import UnoCSSPlugin from 'unocss/vite'
 import { presetAttributify, presetIcons, presetWind3, transformerDirectives, transformerVariantGroup } from 'unocss'
 import lodash from 'es-toolkit/compat'
 import { createMpaPlugin } from 'vite-plugin-virtual-mpa'
+import PugPlugin from 'vite-plugin-pug'
+import * as cheerio from 'cheerio'
 // @ts-ignore
 import pkgInfo from './package.json'
 import { PLATFORM } from './src/pages/Solution/constant'
@@ -71,67 +73,98 @@ const appendExternalModules = (rollupOption: Required<BuildEnvironmentOptions>['
   return rollupOption
 }
 
-export default defineConfig({
-  root: appPath,
-  build: {
-    outDir: outPath,
-    rollupOptions: appendExternalModules({
-      input: path.resolve(__dirname, 'src/demo.html'),
-      output: {
-        manualChunks: {
-          'hls-chunk': ['hls.js'],
-          'vue-chunk': ['vue', '@vueuse/core', 'vue-router'],
-          'i18n-chunk': [
-            path.join(__dirname, 'src/scripts/useI18n.ts'),
-            path.join(__dirname, 'src/scripts/useI18nAsync.ts'),
-            path.join(__dirname, 'src/scripts/useI18nJSON.ts'),
-            path.join(__dirname, 'src/scripts/useI18nJSONAsync.ts'),
-          ],
-          ...genLayoutChunk(['zh-cn', 'en-us', 'zh-hk']),
+const getDemoLinks = async () => {
+  const targetDomain = 'https://demo.swarmcloud.net'
+  const parseUrl = (urlPath?: string) => {
+    if (!urlPath) return ''
+    const url = new URL(urlPath, targetDomain)
+    url.protocol
+    return url.href.replace(url.protocol, '')
+  }
+  const $ = await cheerio.fromURL(targetDomain)
+
+  const links = $('link[href]')
+    .filter((_, el) => {
+      const el$ = $(el)
+      const href = el$.attr('href')
+      if (!href) return false
+      if (el$.attr('rel') === 'icon') return false
+      if (/^(\w+:)?\/\//.test(href)) return false
+      return true
+    })
+    .map((_, el) => ({ href: parseUrl($(el).attr('href')), ref: $(el).prop('rel'), as: $(el).attr('as') }))
+  const scripts = $('body > script[src]')
+    .map((_, el) => ({ src: parseUrl($(el).attr('src')) }))
+
+  return { links: Array.from(links), scripts: Array.from(scripts) }
+}
+
+export default defineConfig(async () => {
+  const { links, scripts } = await getDemoLinks()
+  console.log(links, scripts)
+  return {
+    root: appPath,
+    build: {
+      outDir: outPath,
+      rollupOptions: appendExternalModules({
+        input: path.resolve(__dirname, 'src/demo.html'),
+        output: {
+          manualChunks: {
+            'hls-chunk': ['hls.js'],
+            'vue-chunk': ['vue', '@vueuse/core', 'vue-router'],
+            'i18n-chunk': [
+              path.join(__dirname, 'src/scripts/useI18n.ts'),
+              path.join(__dirname, 'src/scripts/useI18nAsync.ts'),
+              path.join(__dirname, 'src/scripts/useI18nJSON.ts'),
+              path.join(__dirname, 'src/scripts/useI18nJSONAsync.ts'),
+            ],
+            ...genLayoutChunk(['zh-cn', 'en-us', 'zh-hk']),
+          },
+          paths: {
+            'hls.js': isCI ? `https://fastly.jsdelivr.net/npm/hls.js@${getModuleVersion('hls.js')}/dist/hls.mjs` : '',
+          },
         },
-        paths: {
-          'hls.js': isCI ? `https://fastly.jsdelivr.net/npm/hls.js@${getModuleVersion('hls.js')}/dist/hls.mjs` : '',
-        },
-      },
-    }),
-  },
-  plugins: [
-    createMpaPlugin({
-      template: path.resolve(__dirname, 'src/index.html') as any,
-      pages: pages as any[],
-    }) as any,
-    VuePlugin({
-      isProduction: true,
-    }),
-    UnoCSSPlugin({
-      mode: 'vue-scoped',
-      presets: [
-        presetAttributify(),
-        presetWind3(),
-        presetIcons({
-          cdn: 'https://esm.sh/'
-        }),
-      ],
-      transformers: [transformerDirectives(), transformerVariantGroup()],
-      shortcuts: {
-        'flex-row-nowrap': 'flex flex-row flex-nowrap',
-        'flex-col-nowrap': 'flex flex-col flex-nowrap',
-        'flex-row-wrap': 'flex flex-row flex-wrap',
-        'flex-col-wrap': 'flex flex-col flex-wrap',
-        'bg-active': 'bg-orange-500',
-        'bg-hover': 'bg-orange-500/95',
-        'bg-odd': 'bg-[#11191c]',
-        'bg-even': 'bg-[#323131]',
-        'text-active': 'text-orange-500',
-        'text-hover': 'text-orange-100',
-      },
-    }),
-  ],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        silenceDeprecations: ['mixed-decls'],
-      }
+      }),
     },
-  },
+    plugins: [
+      PugPlugin(void 0, { links, scripts }),
+      createMpaPlugin({
+        template: path.resolve(__dirname, 'src/index.html') as any,
+        pages: pages as any[],
+      }) as any,
+      VuePlugin({
+        isProduction: true,
+      }),
+      UnoCSSPlugin({
+        mode: 'vue-scoped',
+        presets: [
+          presetAttributify(),
+          presetWind3(),
+          presetIcons({
+            cdn: 'https://esm.sh/'
+          }),
+        ],
+        transformers: [transformerDirectives(), transformerVariantGroup()],
+        shortcuts: {
+          'flex-row-nowrap': 'flex flex-row flex-nowrap',
+          'flex-col-nowrap': 'flex flex-col flex-nowrap',
+          'flex-row-wrap': 'flex flex-row flex-wrap',
+          'flex-col-wrap': 'flex flex-col flex-wrap',
+          'bg-active': 'bg-orange-500',
+          'bg-hover': 'bg-orange-500/95',
+          'bg-odd': 'bg-[#11191c]',
+          'bg-even': 'bg-[#323131]',
+          'text-active': 'text-orange-500',
+          'text-hover': 'text-orange-100',
+        },
+      }),
+    ],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          silenceDeprecations: ['mixed-decls'],
+        }
+      },
+    },
+  }
 })
